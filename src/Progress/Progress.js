568d3551a41ce9ed6b88d953335bfced7dc7f86e
Julia <138764366+liutiki@users.noreply.github.com>
2024-06-20 14:35:03 +0300

    Update Progress.js

--- src/Progress/Progress.js
+++ src/Progress/Progress.js
@@ -2,25 +2,22 @@ function Progress (){
     return(
         <div>
             <div className="container">
-
-           <div className="circle">
+            <div className="circle">
             <p className="bar">0/7</p>
-           </div>
-
-           <div>
+        </div>
+            <div>
             <div className="checkOne">
             <p classname="checkTwo">Чек лист</p>
             </div>
             <p className="done">Выполнено: 0 из 7 действий</p>
             <p className="next">Следующее действие: Созвониться с клиентом до 23.05.24 12:00</p>
-           </div>
-
-           <div>
-            <button className="cta">Открыть чеклист</button>
-           </div>
             </div>
-   
-    </div>
+
+            <div>
+            <button className="cta">Открыть чеклист</button>
+            </div>
+            </div>
+       </div>
     )
 }
 
